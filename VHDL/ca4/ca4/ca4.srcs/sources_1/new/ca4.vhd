5f40403b2add02df47882e367826222da5c129a5
Benavides <carlos.benavides.86@my.csun.edu>
2019-04-10 10:46:35 -0700

    init ca4

---
+++ VHDL/ca4/ca4/ca4.srcs/sources_1/new/ca4.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/10/2019 10:31:54 AM
+-- Design Name: 
+-- Module Name: ca4 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ca4 is
+--  Port ( );
+end ca4;
+
+architecture Behavioral of ca4 is
+
+begin
+
+
+end Behavioral;
