ca06d9b6909186b92f07af0306be34457c37ff4e
Benavides <carlos.benavides.86@my.csun.edu>
2019-04-22 10:49:58 -0700

    init

--- VHDL/ca4/ca4/ca4.srcs/sources_1/new/ca4.vhd
+++ VHDL/ca4/ca4/ca4.srcs/sources_1/new/ca4.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,12 +32,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity ca4 is
---  Port ( );
+  port(
+            x:      in std_logic_vector(15 downto 0);
+            y:      in std_logic_vector(15 downto 0);
+            clk:    in std_logic;
+            rst:    in std_logic;
+            prod:   out std_logic_vector(31 downto 0)    
+       );
 end ca4;
 
 architecture Behavioral of ca4 is
+signal prod_sig: std_logic_vector(32 downto 0);
+signal y_reg, op1, op2: std_logic_vector(15 downto 0);
+signal op1_sel: std_logic;
 
 begin
+    process(clk, rst)
+    begin
+        if rising_edge(clk) then
+            y_reg <= y;
+        end if;
+    end process;
+
+op1 <= y_reg when op1_sel =  '1' else (others => '0');
+
+prod_sig <= std_logic_vector(unsigned("0000000000000000"&op1) + unsigned("00000000000000000"&op2));
 
 
 end Behavioral;
