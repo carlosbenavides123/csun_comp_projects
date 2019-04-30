684091976c6805c70d6a5e291f1fa5be8e34ffc2
carlosbenavides123 <carlos7legend@gmail.com>
2019-04-29 22:51:15 -0700

    init ca5

---
+++ VHDL/ca5/ca5.srcs/sources_1/new/ca5_top.vhd
@@ -0,0 +1,79 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/29/2019 09:33:29 PM
+-- Design Name: 
+-- Module Name: ca5_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ca5_top is
+  port(
+            clk, rst: in std_logic  
+       );
+end ca5_top;
+
+architecture Behavioral of ca5_top is
+
+    type StateType is (Idle, Grant_A, Grant_B, Grant_C);
+    signal CurrentState, NextState: StateType:= Idle;
+    signal count: integer := 0;
+begin
+
+FSM: process(clk, rst)
+begin
+    if rst = '1' then
+        CurrentState <= Idle;
+    elsif rising_edge(clk) then
+        case CurrentState is
+            when Grant_A =>
+                if count = 60 then
+                    CurrentState <= NextState;
+                    count <= 0;
+               else
+                    count <= count + 1;
+               end if;
+            when Grant_B =>
+                if count = 60 then
+                    CurrentState <= NextState;
+                    count <= 0;
+                else
+                    count <= count + 1;
+                end if;
+            when Grant_C =>
+                if count = 60 then
+                    CurrentState <= NextState;
+                    count <= 0;
+                else
+                    count <= count + 1;
+                end if;
+            when others => 
+                CurrentState <= Idle;
+              end case;
+          end if;
+    end process;
+end Behavioral;
